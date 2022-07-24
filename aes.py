a1f15739c998e5cf0682dd569df04a58bb0aa4a2
Dylan Reuter <djreuter45@gmail.com>
2022-07-24 12:03:07 -0500

    finished AES encrypt

--- aes.py
+++ aes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-import hashlib
+import sys
+import uuid
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
@@ -9,12 +10,17 @@ from Crypto.Random import get_random_bytes
 
 def encrypt(data, padding):
     key = get_random_bytes(16)
-    cipher = AES.new(hashlib.sha256(key).digest(), AES.MODE_CBC)
+    cipher = AES.new(key, AES.MODE_CBC)
     ct = cipher.encrypt(pad(data, AES.block_size))
+    print("IV: ", fmt_output(cipher.iv, padding))
+    print("KEY: ", fmt_output(key, padding))
+    return ct
 
+
+def fmt_output(ct, padding):
     p = '02x' if padding == True else '1x'
-    print('char key[] = { 0x' + ', 0x'.join(format(x, p) for x in key) + '};')
-    print('{ 0x' + ', 0x'.join(format(x, p) for x in ct) + '};')
+    fmt_data = '0x' + ',0x'.join(format(x, p) for x in ct)
+    return fmt_data
 
 
 if __name__ == '__main__':
@@ -25,11 +31,28 @@ if __name__ == '__main__':
     parser.add_argument(
         '--pad', dest='pad', help='Pad with 0 so all output is 2 chars wide 0x0 => 0x00', action=argparse.BooleanOptionalAction, default='--no-pad'
     )
+    parser.add_argument(
+        '-w', help="File to write encrypted binary payload to. If omitted, output it formatted into byte array and saved to as random uuid filename."
+    )
     args = parser.parse_args()
+    if args.f == None:
+        parser.print_help()
+        sys.exit()
 
-    if args.f != None:
-        with open(args.f, mode='rb') as file:
-            buf = file.read()
-            file.close()
+    with open(args.f, mode='rb') as file:
+        buf = file.read()
+        file.close()
         
-        encrypt(buf, args.pad)
+        ciphertext = encrypt(buf, args.pad)
+
+    if args.w != None:
+        with open(args.w, mode='wb') as file:
+            file.write(ciphertext)
+            file.close
+    else:
+        f_name = uuid.uuid4()
+        file = open(f"{f_name}.txt", "w") 
+        file.write(fmt_output(ciphertext, args.pad))
+        file.close()
+        print(f"[+] Encrypted payload saved as {f_name}.txt!")
+        
